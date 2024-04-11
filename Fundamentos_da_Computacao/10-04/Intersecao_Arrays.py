c58259447c9fe399c35535084671f6f7b9120ad3
Gustavo Marinho <101999608+GustavoMarinho82@users.noreply.github.com>
2024-04-11 08:59:03 -0300

    Update Intersecao_Arrays.py

--- Fundamentos_da_Computacao/10-04/Intersecao_Arrays.py
+++ Fundamentos_da_Computacao/10-04/Intersecao_Arrays.py
@@ -9,8 +9,7 @@ for i in range(len(vetor1)):
     vetor2[i] = int(input("Escreva um valor para Vetor2 na coordenada {}\n".format(i)))
     
 for x in vetor1:
-    for y in vetor2:
-        if (x == y) and not (x in vetor_intersecao):
-            vetor_intersecao.append(x)
+    if (x in vetor2) and not (x in vetor_intersecao):
+        vetor_intersecao.append(x)
 
-print("Interceção dos vetores: ", vetor_intersecao)
+print("Vetor1: {} \nVetor2: {} \nInterceção dos vetores: {}".format(vetor1, vetor2, vetor_intersecao))
