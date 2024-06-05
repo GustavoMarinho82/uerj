165e9c5aba92fede7a487c03bae8c312f918bd67
Gustavo Marinho <101999608+GustavoMarinho82@users.noreply.github.com>
2024-06-05 11:28:48 -0300

    commit

--- Fundamentos_da_Computacao/Exercicios/06-05/Ex01.py
+++ Fundamentos_da_Computacao/Exercicios/06-05/Ex01.py
@@ -2,9 +2,9 @@
 
 def conversao_dec_bin(n):
     if (n == 0):
-        return ""
+        return "0"
     else:
-        return str(conversao_dec_bin(n//2)) + str(n%2)
+        return int(str(conversao_dec_bin(n//2)) + str(n%2))
 
 
 print(conversao_dec_bin(13))
